4e2260f3268c5191dc481aeaef76a9adbe1453b2
Niecmano <94400986+Niecmano@users.noreply.github.com>
2023-02-21 23:16:23 +0100

    Add files via upload

---
+++ grep_sa_bojama.c
@@ -0,0 +1,64 @@
+#include <stdio.h> 
+#include <string.h>
+
+    int contains(char red[], char kljucnaRec[]);
+    void sareni_ispis(char red[], char izraz[]);
+
+    int main(int argc, char* argv[]) {
+
+        char fajl[50];
+        strcpy(fajl, argv[2]);
+        char izraz[50];
+        strcpy(izraz, argv[1]);
+        char red[150] = "";
+
+        FILE* fp = fopen(fajl, "r");
+        if (fp == NULL) {
+            printf("Greska! Prosledjeni fajl ne postoji\n");
+            return -1;
+        }
+
+
+        while (fgets(red, sizeof(red), fp)) {
+
+            red[strcspn(red, "\n")] = '\0';
+            if (contains(red, izraz)) sareni_ispis(red, izraz); //printf("%s\n", red);
+        }
+
+        fclose(fp);
+        return 0;
+    }
+
+    int contains(char red[], char izraz[]) {
+
+        int n1 = strlen(red);
+        int n2 = strlen(izraz);
+        int i, j;
+
+        for (i = 0; i <= n1 - n2; i++) {
+            for (j = 0; j < n2; j++) {
+                if (red[i + j] != izraz[j]) break;
+            }
+
+            if (j == n2) return 1;
+        }
+        return 0;
+    }
+
+    void sareni_ispis(char alpha[], char beta[])
+    {
+        char before[100], after[100];
+
+        int pos = strstr(alpha, beta) - alpha;
+
+        strncpy(before, alpha, pos);
+        before[pos] = '\0';
+
+        strcpy(after, alpha + pos + strlen(beta));
+  
+        printf("%s", before);
+        printf("\033[1;31m"); 
+        printf("%s", beta);
+        printf("\033[0m");
+        printf("%s\n", after);
+    }
